4b3c67c5a3ef93489928edb9701cfb334ef9c105
André Luiz Vicente <andre.vicente@zup.com.br>
2021-08-24 13:58:04 -0300

    Primeiro Exemplo de hoje 24/08

---
+++ src/br/com/zup/Aula1_Exemplo1.java
@@ -0,0 +1,19 @@
+package br.com.zup;
+
+public class Aula1_Exemplo1 {
+
+    public static void main(String[] args) {
+
+        int idade_do_usuario = 700;
+        String nomeDoUsuario = "Xablau";
+
+        System.out.print("Olá, meu nome é ");
+        System.out.print(nomeDoUsuario);
+        System.out.print(" e minha idade é ");
+        System.out.print(idade_do_usuario);
+        System.out.print(" anos");
+
+    }
+
+
+}
