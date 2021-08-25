5dbd32d306ebf4176e94e7e0da4c818ed9ff274c
André Luiz Vicente <andre.vicente@zup.com.br>
2021-08-25 14:05:25 -0300

    Colocando o Scanner no exercicio de media arit.

---
+++ src/br/com/zup/Aula2_Exemplo3.java
@@ -0,0 +1,40 @@
+package br.com.zup;
+
+// Faça um programa que receba 4 notas bimestrais e mostre a média aritmética.
+
+import java.util.Scanner;
+
+public class Aula2_Exemplo3 {
+    public static void main(String[] args) {
+        // Instanciar o Scanner
+        Scanner obj_leitor = new Scanner(System.in);
+
+        // Declarar o valor de todas as variáveis
+        float nota1;
+        float nota2;
+        float nota3;
+        float nota4;
+        float soma;
+        float media;
+
+        // Receber o valor das variáveis
+        System.out.println("Vou calcular a média aritmética das notas para você.");
+        System.out.println("Por favor, digite a nota 1: ");
+        nota1 = obj_leitor.nextFloat();
+        System.out.println("Por favor, digite a nota 2: ");
+        nota2 = obj_leitor.nextFloat();
+        System.out.println("Por favor, digite a nota 3: ");
+        nota3 = obj_leitor.nextFloat();
+        System.out.println("Por favor, digite a nota 4: ");
+        nota4 = obj_leitor.nextFloat();
+
+        // Processar os dados
+        soma = nota1 + nota2 + nota3 + nota4;
+        media = soma/4;
+
+        // Exibir os dados
+        System.out.println("A média aritmética de todas as notas é: ");
+        System.out.println(media);
+
+    }
+}
