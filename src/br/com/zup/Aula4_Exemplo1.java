9f9a94bce793dfe6a81c76887406fd1ed6b222e1
André Luiz Vicente <andre.vicente@zup.com.br>
2021-08-27 12:06:44 -0300

    Resolvendo exercicio 4 da lista 2 com impressao de porcentagem

--- src/br/com/zup/Aula4_Exemplo1.java
+++ src/br/com/zup/Aula4_Exemplo1.java
@@ -8,35 +8,50 @@ public class Aula4_Exemplo1 {
     public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in);
 
+        System.out.println("Salários até R$ 280,00 (incluindo), receberão aumento de 20%");
+        System.out.println("Salários entre R$ 280,00 e R$ 700,00 (incluindo), receberão aumento de 15%");
+        System.out.println("Salários entre R$ 700,00 e R$ 1500,00 (incluindo), receberão aumento de 10%");
+        System.out.println("Salários de R$ 1500,00 em diante, receberão aumento de 5% ");
+
         System.out.println("Por favor, digite o seu salário: ");
+
         double salarioAtual = leitor.nextDouble();
-        // double percentualDeAumento;
+        double percentualDeAumento = 0;
         double valorDoAumento = 0;
         double novoSalario;
 
+
+
         if (salarioAtual > 0 & salarioAtual <= 280){
-            System.out.println("Você recebeu um aumento de 20% ");
-            valorDoAumento = salarioAtual * 0.2;
+            //System.out.println("Você recebeu um aumento de 20% ");
+            percentualDeAumento = 0.2;
         }
+
         else if (salarioAtual > 280 & salarioAtual <= 700){
-            System.out.println("Você recebeu um aumento de 15%");
-            valorDoAumento = salarioAtual * 0.15;
+           // System.out.println("Você recebeu um aumento de 15%");
+            percentualDeAumento = 0.15;
         }
+
         else if (salarioAtual > 700 & salarioAtual <= 1500){
-            System.out.println("Você recebeu um aumento de 10%");
-            valorDoAumento = salarioAtual * 0.1;
+           // System.out.println("Você recebeu um aumento de 10%");
+            percentualDeAumento = 0.1;
         }
+
         else if(salarioAtual > 1500){
-            System.out.println("Você recebeu um aumento de 5%");
-            valorDoAumento = salarioAtual * 0.05;
+          //  System.out.println("Você recebeu um aumento de 5%");
+            percentualDeAumento = 0.05;
         }
+
         else{
             System.out.println("Por favor, digite um valor maior que 0");
         }
 
+        valorDoAumento = salarioAtual * percentualDeAumento;
         novoSalario = salarioAtual + valorDoAumento;
 
+
         System.out.println("O salário atual da pessoa é: R$ " +salarioAtual);
+        System.out.println("Você receberá " + (percentualDeAumento * 100)  + "% de aumento");
         System.out.println("O valor do seu aumento será: R$ "+valorDoAumento);
         System.out.println("O seu novo salário será: R$ "+novoSalario);
     }
