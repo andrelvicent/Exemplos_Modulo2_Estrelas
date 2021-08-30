b81878ee9268b5c374a27f2d154bcbb3995b7f86
André Luiz Vicente <andre.vicente@zup.com.br>
2021-08-30 13:53:16 -0300

    Criando loop para somar entrada de dados

---
+++ src/br/com/zup/Aula5/Aula5_Exemplo3.java
@@ -0,0 +1,30 @@
+package br.com.zup.Aula5;
+
+import java.util.Scanner;
+
+// Escreva, usando while, um programa para calcular a média de N números. O valor de N é dado pelo usuário.
+public class Aula5_Exemplo3 {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+
+        double quantidadeDeNumeros;
+        double soma = 0;
+        double media = 0;
+        int contador = 0;
+        System.out.println("Por favor, digite a quantidade de números: ");
+        quantidadeDeNumeros = leitor.nextDouble();
+
+        while (contador < quantidadeDeNumeros){
+            System.out.println("Digite um número: ");
+            double valorLidoPeloUsuario = leitor.nextDouble();
+            // Toda vez que executar o loop, nós vamos somar o valor lido a variável soma
+            soma = soma + valorLidoPeloUsuario;
+
+            System.out.println("Valor da variável soma até agora: "+soma);
+            contador ++;
+        }
+        System.out.println("A soma de todos os números é: " +soma);
+    }
+
+
+}
