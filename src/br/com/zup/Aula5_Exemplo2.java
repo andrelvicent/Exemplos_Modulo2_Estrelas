6e59eff6fb3b750bfff01eb94dedd10dc242368b
André Luiz Vicente <andre.vicente@zup.com.br>
2021-08-30 11:47:24 -0300

    Estrutura de repetição do-while

--- src/br/com/zup/Aula5_Exemplo2.java
+++ src/br/com/zup/Aula5_Exemplo2.java
@@ -1,5 +1,7 @@
 package br.com.zup;
 
+import jdk.swing.interop.SwingInterOpUtils;
+
 public class Aula5_Exemplo2 {
     public static void main(String[] args) {
      //   System.out.println("0");
@@ -7,12 +9,18 @@ public class Aula5_Exemplo2 {
       //  System.out.println("2");
      //   System.out.println("3");
      //   System.out.println("4");
-        int contador = 0;
+        int contador = -2;
 
-        // Criando uma estrutura de repetição
-        while (contador <= 1000){
-            System.out.println("André é bonito ");
-            contador += 1;
-        }
+    //    while (contador >= 0){
+    //        System.out.println(contador);
+    //        contador += 1;
+    //    }
+
+        do {
+            System.out.println(contador);
+            contador ++;
+            System.out.println(contador);
+        }while (contador >= 0);
+
     }
 }
